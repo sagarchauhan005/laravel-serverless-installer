c0a8cf78d0119843370e07c011cee4716ec78435
Sagar Chauhan <coolsagar005@gmail.com>
2021-09-16 16:22:22 +0530

    service provider

---
+++ src/LaravelServerlessServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace sagarchauhan\LaravelServerlessServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelServerlessServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+            $this->publishes([
+                __DIR__.'/sagar-laravel-serverless-installer.sh' =>  app()->basePath() . '/sagar-laravel-serverless-installer.sh',
+            ]);
+        }
+    }
+}
